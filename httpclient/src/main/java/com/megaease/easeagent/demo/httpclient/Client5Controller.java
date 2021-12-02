cd44c565a8046b4f1dec288e9af208e3b7a8d590
yufu.deng <observeralone@gmail.com>
2021-12-02 14:37:34 +0800

    add client5 and ok client

---
+++ httpclient/src/main/java/com/megaease/easeagent/demo/httpclient/Client5Controller.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.megaease.easeagent.demo.httpclient;
+
+import org.apache.hc.client5.http.async.methods.*;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.message.StatusLine;
+import org.apache.hc.core5.reactor.IOReactorConfig;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@RestController
+public class Client5Controller {
+    private final CloseableHttpAsyncClient client;
+
+    public Client5Controller() {
+        final IOReactorConfig ioReactorConfig = IOReactorConfig.custom()
+                .setSoTimeout(Timeout.ofSeconds(5))
+                .build();
+
+        this.client = HttpAsyncClients.custom()
+                .setIOReactorConfig(ioReactorConfig)
+                .build();
+
+        client.start();
+    }
+
+    @GetMapping("/client5")
+    public String client5() throws IOException, ParseException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(ExampleUrl.url);
+            // The underlying HTTP connection is still held by the response object
+            // to allow the response content to be streamed directly from the network socket.
+            // In order to ensure correct deallocation of system resources
+            // the user MUST call CloseableHttpResponse#close() from a finally clause.
+            // Please note that if response content is not fully consumed the underlying
+            // connection cannot be safely re-used and will be shut down and discarded
+            // by the connection manager.
+            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+                System.out.println(response1.getCode() + " " + response1.getReasonPhrase());
+                HttpEntity entity1 = response1.getEntity();
+                return EntityUtils.toString(entity1);
+            }
+        }
+    }
+
+    @GetMapping("/client5_async")
+    public String client5Async() throws IOException, ParseException, ExecutionException, InterruptedException {
+        final HttpHost target = new HttpHost("127.0.0.1", 8500);
+        final String requestUri = "/example";
+        final SimpleHttpRequest request = SimpleRequestBuilder.get()
+                .setHttpHost(target)
+                .setPath(requestUri)
+                .build();
+        System.out.println("Executing request " + request);
+        final Future<SimpleHttpResponse> future = client.execute(
+                SimpleRequestProducer.create(request),
+                SimpleResponseConsumer.create(),
+                new FutureCallback<SimpleHttpResponse>() {
+
+                    @Override
+                    public void completed(final SimpleHttpResponse response) {
+                        System.out.println(request + "->" + new StatusLine(response));
+                        System.out.println(response.getBody());
+                    }
+
+                    @Override
+                    public void failed(final Exception ex) {
+                        System.out.println(request + "->" + ex);
+                    }
+
+                    @Override
+                    public void cancelled() {
+                        System.out.println(request + " cancelled");
+                    }
+
+                });
+        SimpleHttpResponse simpleHttpResponse = future.get();
+        return simpleHttpResponse.getBodyText();
+    }
+}
---
+++ httpclient/src/main/java/com/megaease/easeagent/demo/httpclient/OkClientController.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.megaease.easeagent.demo.httpclient;
+
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+@RestController
+public class OkClientController {
+    private final AtomicLong counter = new AtomicLong();
+    OkHttpClient client = new OkHttpClient();
+
+    @GetMapping("/okclient")
+    public String client() throws IOException {
+        Request request = new Request.Builder()
+                .url(ExampleUrl.url)
+                .build();
+
+        Call call = client.newCall(request);
+        try (Response response = call.execute()) {
+            return String.format("count : %s  hello : %s", counter.incrementAndGet(), response.body().string());
+        }
+    }
+
+    @GetMapping("/okclient_async")
+    public String clientAsync() throws IOException, InterruptedException {
+        Request request = new Request.Builder()
+                .url(ExampleUrl.url)
+                .build();
+
+        Call call = client.newCall(request);
+        final AtomicReference<String> reference = new AtomicReference<>();
+        Callback callback = new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                synchronized (reference) {
+                    reference.set(e.getMessage());
+                    reference.notifyAll();
+                }
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                synchronized (reference) {
+                    reference.set(String.format("count : %s  hello : %s", counter.incrementAndGet(), response.body().string()));
+                    reference.notifyAll();
+                }
+            }
+        };
+        call.enqueue(callback);
+        synchronized (reference) {
+            if (reference.get() == null) {
+                reference.wait();
+            }
+        }
+        return reference.get();
+    }
+
+}
