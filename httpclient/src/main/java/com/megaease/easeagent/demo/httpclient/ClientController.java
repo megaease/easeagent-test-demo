062f6a596493feb3f8bbbe33fa3dc5716c3c6e50
zhoujinze <zhoujinze@gmail.com>
2021-12-23 10:35:44 +0800

    update plugin to v2.0.0

--- httpclient/src/main/java/com/megaease/easeagent/demo/httpclient/ClientController.java
+++ httpclient/src/main/java/com/megaease/easeagent/demo/httpclient/ClientController.java
@@ -25,6 +25,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,6 +63,9 @@ public class ClientController {
                     context = EntityUtils.toString(entity);
                 }
                 System.out.println("------------------------------------");
+                Logger alog = LoggerFactory.getLogger("app");
+                org.slf4j.MDC.put("testMDC", "bbb");
+                alog.info("-------------------alog---");
             } finally {
                 response.close();
             }
