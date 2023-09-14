2d436d5fa6f18e5172b59b5d0262ba2632f9979b
enoch <lanxenet@gmail.com>
2023-09-14 14:32:15 +0800

    feat: add httpclient HttpURLConnection demo

--- httpclient/curl_add_header.sh
+++ httpclient/curl_add_header.sh
@@ -20,3 +20,4 @@ curl -H "root_source:curl" http://127.0.0.1:8080/client5
 curl -H "root_source:curl" http://127.0.0.1:8080/client5_async
 curl -H "root_source:curl" http://127.0.0.1:8080/okclient
 curl -H "root_source:curl" http://127.0.0.1:8080/okclient_async
+curl -H "root_source:curl" http://127.0.0.1:8080/urlconnection
